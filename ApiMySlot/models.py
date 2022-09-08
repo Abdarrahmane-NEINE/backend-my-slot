90ab6ccaf7978a5c69713d6239bc6e3c7795a7c3
Abdarrahmane NEINE <abdarrahmane@MacBook-Pro-de-Abdarrahmane.local>
2022-09-08 12:37:41 +0200

    validate reservation crud

--- ApiMySlot/models.py
+++ ApiMySlot/models.py
@@ -3,12 +3,8 @@ import datetime
 
 class Availabilities(models.Model):
     Id = models.AutoField(primary_key=True)
-    # Start = models.DateTimeField("%mm.%d.%YYYY %h:%mm aa")
-    # Start = models.DateTimeField("%Y-%m-%d %I")
     Start = models.DateTimeField("%Y-%m-%d %h:%m")
-    # Start = models.DateTimeField(default=timezone.now)
     Start.editable= True
-    # End = models.DateTimeField("%mm.%d.%YYYY %h:%mm aa")
     End = models.DateTimeField("%Y-%m-%d  %h:%m")
     End.editable= True
     CreatedAt = models.DateTimeField(auto_now=True)
@@ -17,11 +13,11 @@ class Reservations(models.Model):
     Id = models.AutoField(primary_key=True)
     Title = models.CharField(max_length=500)
     Email = models.EmailField(max_length=254)
-    Start = models.DateTimeField(auto_now=False)
+    Start = models.DateTimeField("%Y-%m-%d %h:%m")
     Start.editable= True
-    End = models.DateTimeField(auto_now=False)
+    End = models.DateTimeField("%Y-%m-%d %h:%m")
     End.editable= True
-    CreatedAt = models.DateTimeField(auto_now=True)
+    CreatedAt = models.DateTimeField("%Y-%m-%d %h:%m")
 
 class Tests(models.Model):
     Id = models.AutoField(primary_key=True)
