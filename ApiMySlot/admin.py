1a24a0f6f2d7348b1e8014fdbf634e870b68c46f
Abdarrahmane-NEINE <kaberneine10@gmail.com>
2025-01-11 19:40:49 +0100

    show model in admin panel

--- ApiMySlot/admin.py
+++ ApiMySlot/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import Availabilities, Reservations, Tests
+# models
+admin.site.register(Availabilities)
+admin.site.register(Reservations)
+admin.site.register(Tests)
